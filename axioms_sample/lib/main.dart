6d17c042ae9c0d626de834afcdaeb6e7d756f2a7
sahildmk <z5255280@ad.unsw.edu.au>
2020-05-17 15:27:03 +1000

    Login Page

--- axioms_sample/lib/main.dart
+++ axioms_sample/lib/main.dart
@@ -11,14 +11,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromRGBO(1, 46, 102, 100),
       body: Container(
         child: Column(
-          children: <Widget> [
-            Container(
-
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
+                    text: 'axioms',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Nunito',
+                      fontSize: 70,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontFamily: 'Sen',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                )
+              ],
             )
-          ]
+          ],
         ),
       ),
     );
