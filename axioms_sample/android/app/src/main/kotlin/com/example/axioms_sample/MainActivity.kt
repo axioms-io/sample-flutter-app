dedd3edb5de864bd67969e7626511e9274169590
sahildmk <z5255280@ad.unsw.edu.au>
2020-05-24 00:28:39 +1000

    run-fix

--- axioms_sample/android/app/src/main/kotlin/com/example/axioms_sample/MainActivity.kt
+++ axioms_sample/android/app/src/main/kotlin/com/example/axioms_sample/MainActivity.kt
@@ -1,15 +1,6 @@
 package com.example.axioms_sample
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-import android.content.Intent;
-import android.os.Bundle;
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
