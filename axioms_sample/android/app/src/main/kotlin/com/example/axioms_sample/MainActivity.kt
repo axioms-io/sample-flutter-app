09d176d4dd45e5559b63892570020e0efc0a9e08
sahildmk <z5255280@ad.unsw.edu.au>
2020-05-23 17:57:24 +1000

    Making progress on receiving URI links

--- axioms_sample/android/app/src/main/kotlin/com/example/axioms_sample/MainActivity.kt
+++ axioms_sample/android/app/src/main/kotlin/com/example/axioms_sample/MainActivity.kt
@@ -5,6 +5,9 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+import android.content.Intent;
+import android.os.Bundle;
+
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
--- axioms_sample/lib/main.dart
+++ axioms_sample/lib/main.dart
@@ -1,16 +1,66 @@
 import 'dart:async';
-import 'dart:html';
+// import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:uni_links/uni_links.dart';
+import 'package:flutter/services.dart' show PlatformException;
+
 void main() => runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomePage(),
+    home: Container(
+      child: UriLinks(),
+    ),
   )
 );
+
+class UriLinks extends StatefulWidget {
+  UriState createState() => new UriState();
+}
+
+class UriState extends State with SingleTickerProviderStateMixin {
+  StreamSubscription _sub;
+
+  Future<Null> initUniLinks() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      String initialLink = await getInitialLink();
+      print("Some link");
+    } on PlatformException {
+      print("Not Successful");
+    }
+
+    _sub = getLinksStream().listen((String link) {
+      print("Recieved Link");
+    }, onError: (err) {
+      print("Error");
+    });
+  }
+
+  @override
+  void initState() {
+    initUniLinks();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+
+  @override
+  dispose() {
+    if (_sub != null) {
+      print("Canceling Sub");
+      _sub.cancel();
+    }
+    super.dispose();
+  }
+
+}
 
 class WebBrowser extends StatelessWidget {
   final Completer<WebViewController> _controller = Completer<WebViewController>();
@@ -42,8 +92,6 @@ class WebBrowser extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -96,7 +144,7 @@ class HomePage extends StatelessWidget {
                   ),
                 )
               ],
-            )
+            ),
           ],
         ),
       ),
