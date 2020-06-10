fa724c46376f48c12982818d83110fb07e12cc88
sahildmk <z5255280@ad.unsw.edu.au>
2020-06-11 00:11:53 +1000

    Created Auth Class and URL functions

--- axioms_sample/lib/auth.dart
+++ axioms_sample/lib/auth.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:convert' as convert;
+import 'package:nanoid/nanoid.dart';
+import 'package:http/http.dart';
 class Auth {
   String axioms_domain;
   String response_type;
@@ -7,6 +11,54 @@ class Auth {
   String login_scope;            
   String login_type;  
   String post_login_navigate;
-}
+  String nonce;
+  String state;
+  String challenge;
+  String challenge_verifier;
+  String url;
+  String authPath = '/oauth2/authorize';
 
-// Session Class
+  Auth(String axioms_domain, String response_type, String redirect_uri, String client_id, String login_scope) {
+    this.axioms_domain = axioms_domain;
+    this.response_type = response_type;
+    this.redirect_uri = redirect_uri;
+    this.client_id = client_id;
+    this.login_scope = login_scope;
+    this.nonce = nanoid();
+    this.state = nanoid();
+    buildUrl();
+  }
+
+  String getState() {
+    return this.state;
+  }
+
+  void buildUrl() {
+    var queryParams = {
+      "response_type": this.response_type,
+      "client_id": this.client_id,
+      "redirect_uri": this.redirect_uri,
+      "scope": this.login_scope,
+      "state": this.state,
+      "nonce": this.nonce
+    };
+    // Uri link = new Uri.https(this.axioms_domain, this.authPath, queryParams);
+    // this.url = link.toString();
+    String link = "https://" + this.axioms_domain + authPath;
+    for (int i = 0; i < queryParams.keys.length; i++) {
+      String key = queryParams.keys.toList()[i];
+      String current;
+      if (i == 0) {
+        current = "?" + key + "=" + queryParams[key];
+      } else {
+        current = "&" + key + "=" + queryParams[key];
+      }
+      link += current;
+    }
+    this.url = link;
+  }
+
+  String getUrl() {
+    return this.url;
+  }
+}
